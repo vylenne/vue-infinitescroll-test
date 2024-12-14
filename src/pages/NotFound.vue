f21f78aeed8841573dcabdd6305ac1f831b6ea7c
vylenne <v.vylenne@gmail.com>
2024-12-14 16:08:59 +0400

    Notfound page styling

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="NotFoundPage">
     <h1>404 - Page Not Found</h1>
     <p>The page you are looking for does not exist.</p>
     <router-link to="/">
@@ -13,3 +13,13 @@ export default {
   name: 'NotFound',
 };
 </script>
+
+<style scoped>
+.NotFoundPage {
+  padding-top: 32px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  align-items: center;
+}
+</style>
